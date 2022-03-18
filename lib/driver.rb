6f4c5cc3add91f8a02bfa0a74de2de624533c03d
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-03-17 19:10:31 -0700

    add unit tests for tile_set.rb

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,5 +2,4 @@
 require_relative 'game'
 
 game = Game.new
-
 game.play
--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -5,15 +5,21 @@ describe 'A tile set' do
   describe '::generate' do
 
     it 'returns an array of 9 tiles by default' do
-      raise "Start here"
+      tile_set = TileSet.generate
+      expect(tile_set.length).to eq(9)
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      tile_set = TileSet.generate(10)
+      expect(tile_set.length).to eq(10)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
-      skip
+      i = 0
+      tile_set = TileSet.generate
+      tile_set.each do |tile|
+        expect(tile.value).to eq(i += 1)
+      end
     end
 
   end
