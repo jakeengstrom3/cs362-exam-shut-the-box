9b6fb60c14b2b68e42be546a9877efa3bf441a2c
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-03-17 19:51:50 -0700

    add more unit tests to box_spec.rb and add let block for box object in box_spec and tile object in tile_spec

--- spec/box_spec.rb
+++ spec/box_spec.rb
@@ -2,6 +2,8 @@ require_relative '../lib/box'
 
 describe 'A box' do
 
+  let(:box) {Box.new}
+
   describe 'attributes' do
     it 'has tiles' do
       Box.new.tiles
@@ -11,55 +13,80 @@ describe 'A box' do
   describe 'creation' do
     context 'by default' do
       it 'will initialize an array of tiles' do
-        skip
+        
+        expect(box.tiles).to be_a(Array)
       end
     end
     context 'with a specific array of tiles' do
       it 'will retain the tiles passed its constructor' do
-        skip
+        fake_tiles = ["FAKE_TILE#1", "FAKE_TILE#2"]
+        box = Box.new(fake_tiles)
+        expect(box.tiles).to eq(fake_tiles)
       end
     end
   end
 
   describe '#tiles_remaining?' do
     it 'returns true when at least one tile is up' do
-      skip
+      
+      expect(box.tiles_remaining?).to be(true)
     end
     it 'returns false when none of the tiles are up' do
-      skip
+      
+      box.tiles.each do |tile|
+        tile.flip
+      end
+      expect(box.tiles_remaining?). to be(false)
     end
   end
 
   describe '#no_tiles_remaining?' do
     it 'returns true when none of the tiles are up' do
-      skip
+      
+      box.tiles.each do |tile|
+        tile.flip
+      end
+      expect(box.no_tiles_remaining?). to be(true)
     end
     it 'returns false when any tile is up' do
-      skip
+      expect(box.no_tiles_remaining?).to be(false)
     end
   end
 
   describe '#shut?' do
     it 'is true when there are no tiles remaining' do
-      skip
+      
+      box.tiles.each do |tile|
+        tile.flip
+      end
+      expect(box.shut?). to be(true)
     end
     it 'is false when there are tiles remaining' do
-      skip
+      expect(box.shut?).to be(false)
     end
   end
 
   describe '#total_value_of_remaining_tiles' do
     it 'is the sum of the values of all the tiles that are still up' do
-      skip
+      sum = 0
+      box.tiles.each do |tile|
+        sum += tile.value if tile.up?
+      end
+      expect(box.total_value_of_remaining_tiles).to eq(sum)
     end
   end
 
   describe '#flip' do
     it 'flips each tile whose value matches any of the tile values it receives' do
-      skip
+      box.flip([1, 2])
+      expect(box.tiles[0].up?).to be(false)
+      expect(box.tiles[1].up?).to be(false)
+      
+
     end
     it 'does not flip any tile whose value does not match any of the tile values it receives' do
-      skip
+      box.flip([1, 2])
+      expect(box.tiles[2].up?).to be(true)
     end
   end
 
--- spec/tile_spec.rb
+++ spec/tile_spec.rb
@@ -2,29 +2,28 @@ require_relative '../lib/tile'
 
 describe 'A tile' do
 
+  let(:tile) {Tile.new(1)}
+
   describe 'attributes' do
 
     it 'has a value' do
-      Tile.new(1).value
+      tile.value
     end
-
   end
 
   describe '#up?' do
     it 'is up by default' do
-      expect(Tile.new(1).up?).to be(true)
+      expect(tile.up?).to be(true)
     end
   end
 
   describe '#flip' do
     it 'is up after being flipped' do
-      tile = Tile.new(1)
       tile.flip
       expect(tile.up?).to be(false)
     end
 
     it 'stays down despite being flipped more than once' do
-      tile = Tile.new(1)
       tile.flip
       tile.flip
       expect(tile.up?).to be(false)
@@ -37,13 +36,12 @@ describe 'A tile' do
   describe '#to_s string representation' do
     context 'when up' do
       it 'shows its value within brackets' do
-        expect(Tile.new(1).to_s).to eq("[1]")
+        expect(tile.to_s).to eq("[1]")
       end
     end
 
     context 'when down' do
       it 'shows a space within brackets' do
-        tile = Tile.new(1)
         tile.flip
         expect(tile.to_s).to eq("[ ]")
       end
